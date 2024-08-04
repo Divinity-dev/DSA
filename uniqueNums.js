3369170274f77e9ecbcad1415908b808776c2f75
Divinity-dev <divine_asiriuwa@yahoo.com>
2024-08-04 21:10:15 +0100

    Fix uniqueNums challenge

--- uniqueNums.js
+++ uniqueNums.js
@@ -10,8 +10,18 @@ You can return the array with its elements in any order.
 Input: nums1 = [1,2,3], nums2 = [2,4,6]
 Output: [[1,3],[4,6]]
 */
-const uniqueNums = (arr) => {
-    const set = new Set(arr)
-    return Array.from(set).length;
+// const uniqueNums = (arr) => {
+//     const set = new Set(arr)
+//     return Array.from(set).length;
+// }
+// console.log(uniqueNums([0,0,1,1,1,2,2,3,3,4]))
+const uniqueNums = (nums1, nums2)=>{
+ for(let x of nums1){
+    if(nums2.includes(x)){
+        nums1.splice(nums1.indexOf(x),1)
+        nums2.splice(nums2.indexOf(x),1)
+    }
+ }
+ return [nums1,nums2]
 }
-console.log(uniqueNums([0,0,1,1,1,2,2,3,3,4]))
+console.log(uniqueNums([1,2,3], [2,4,6]))
